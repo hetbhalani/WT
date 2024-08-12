a74f704c6206690c4ebcaf1999beaf245170662e
hetbhalani <bhalanihet2006@gmail.com>
2024-08-12 13:30:29 +0530

    updated

--- test.js
+++ test.js
@@ -3,12 +3,11 @@ const fs = require('fs')
 const child_process = require('child_process')
 const { stdin } = require('process')
 
-// console.log(path.dirname('C:\\Users\\LENOVO\\OneDrive\\Desktop\\SEM-3\\WT'))
-// console.log(path.resolve('C:\\Users\\LENOVO\\OneDrive\\Desktop\\SEM-3\\WT','\\git-bash.lnk'))
-// fs.stat('test.js', (err,data)=>{
-//     console.log(data)
-// })
-
-child_process.exec('start chrome',(err,stdout,stdin)=>{
-    console.log(stdout);
+console.log(path.dirname('C:\\Users\\LENOVO\\OneDrive\\Desktop\\SEM-3\\WT'))
+console.log(path.resolve('C:\\Users\\LENOVO\\OneDrive\\Desktop\\SEM-3\\WT','\\git-bash.lnk'))
+fs.stat('test.js', (err,data)=>{
+    console.log(data)
 })
+
+let a = 2 + 3
+console.log(a);
